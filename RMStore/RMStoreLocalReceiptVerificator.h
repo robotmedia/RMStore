4eda6de83b33fbfb2fb69663820a14505563d3b2
Hermes Pique <hermes.pique@robotmedia.net>
2013-07-31 12:34:26 +0200

    RMStoreLocalReceiptVerificator

---
+++ RMStore/RMStoreLocalReceiptVerificator.h
@@ -0,0 +1,14 @@
+//
+//  RMStoreLocalReceiptVerificator.h
+//  RMStore
+//
+//  Created by Hermes Pique on 7/31/13.
+//  Copyright (c) 2013 Robot Media. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "RMStore.h"
+
+@interface RMStoreLocalReceiptVerificator : NSObject<RMStoreReceiptVerificator>
+
+@end
