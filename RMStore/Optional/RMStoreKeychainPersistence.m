0f11d001f74972a64a2bcd7fb0601b4432518dab
hpique <hermes.pique@robotmedia.net>
2013-10-19 21:29:46 +0200

    First go at RMStoreKeychainPersistence

---
+++ RMStore/Optional/RMStoreKeychainPersistence.m
@@ -0,0 +1,172 @@
+//
+//  RMStoreKeychainPersistence.m
+//  RMStore
+//
+//  Created by Hermes on 10/19/13.
+//  Copyright (c) 2013 Robot Media. All rights reserved.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+#import "RMStoreKeychainPersistence.h"
+#import <Security/Security.h>
+
+NSString* const RMStoreTransactionsKeychainKey = @"RMStoreTransactions";
+
+#pragma mark - Keychain
+
+NSMutableDictionary* RMKeychainGetSearchDictionary(NSString *key)
+{
+    NSMutableDictionary *dictionary = [NSMutableDictionary dictionary];
+    [dictionary setObject:(__bridge id)kSecClassGenericPassword forKey:(__bridge id)kSecClass];
+    
+    NSData *encodedIdentifier = [key dataUsingEncoding:NSUTF8StringEncoding];
+    
+    [dictionary setObject:encodedIdentifier forKey:(__bridge id)kSecAttrGeneric];
+    [dictionary setObject:encodedIdentifier forKey:(__bridge id)kSecAttrAccount];
+    
+    NSString *serviceName = [NSBundle mainBundle].bundleIdentifier;
+    [dictionary setObject:serviceName forKey:(__bridge id)kSecAttrService];
+    
+    return dictionary;
+}
+
+void RMKeychainSetValue(NSData *value, NSString *key)
+{
+    NSMutableDictionary *searchDictionary = RMKeychainGetSearchDictionary(key);
+    OSStatus status;
+    CFTypeRef ignore;
+    if (SecItemCopyMatching((__bridge CFDictionaryRef)searchDictionary, &ignore) == errSecSuccess)
+    { // Update
+        if (!value)
+        {
+            status = SecItemDelete((__bridge CFDictionaryRef)searchDictionary);
+        } else {
+            NSMutableDictionary *updateDictionary = [NSMutableDictionary dictionary];
+            [updateDictionary setObject:value forKey:(__bridge id)kSecValueData];
+            status = SecItemUpdate((__bridge CFDictionaryRef)searchDictionary, (__bridge CFDictionaryRef)updateDictionary);
+        }
+        NSCAssert(status == errSecSuccess, @"failed to update key %@ with error %ld.", key, status);
+    }
+    else if (value)
+    { // Add
+        [searchDictionary setObject:value forKey:(__bridge id)kSecValueData];
+        status = SecItemAdd((__bridge CFDictionaryRef)searchDictionary, NULL);
+        
+        NSCAssert(status == errSecSuccess, @"failed to add key %@ with error %ld.", key, status);
+    }
+}
+
+NSData* RMKeychainGetValue(NSString *key)
+{
+    NSMutableDictionary *searchDictionary = RMKeychainGetSearchDictionary(key);
+    [searchDictionary setObject:(__bridge id)kSecMatchLimitOne forKey:(__bridge id)kSecMatchLimit];
+    [searchDictionary setObject:(id)kCFBooleanTrue forKey:(__bridge id)kSecReturnData];
+    
+    CFDataRef value = nil;
+    OSStatus status = SecItemCopyMatching((__bridge CFDictionaryRef)searchDictionary, (CFTypeRef *)&value);
+    
+    NSCAssert(status == errSecSuccess || status == errSecItemNotFound, @"failed to add key %@ with error %ld.", key, status);
+        
+    return (__bridge NSData*)value;
+}
+
+@implementation RMStoreKeychainPersistence
+
+#pragma mark - RMStoreTransactionPersistor
+
+- (void)persistTransaction:(SKPaymentTransaction*)paymentTransaction
+{
+    SKPayment *payment = paymentTransaction.payment;
+    NSString *productIdentifier = payment.productIdentifier;
+    NSDictionary *transactions = [RMStoreKeychainPersistence transactionsDictionary];
+    NSInteger count = [[transactions objectForKey:productIdentifier] integerValue];
+    count++;
+    NSMutableDictionary *updatedTransactions = [NSMutableDictionary dictionaryWithDictionary:transactions];
+    [updatedTransactions setObject:@(count) forKey:productIdentifier];
+    [RMStoreKeychainPersistence setTransactionsDictionary:updatedTransactions];
+}
+
+#pragma mark - Public
+
+- (void)removeTransactions
+{
+    [RMStoreKeychainPersistence setTransactionsDictionary:nil];
+}
+
+- (BOOL)consumeProductOfIdentifier:(NSString*)productIdentifier
+{
+    NSDictionary *transactions = [RMStoreKeychainPersistence transactionsDictionary];
+    NSInteger count = [[transactions objectForKey:productIdentifier] integerValue];
+    if (count > 0)
+    {
+        count--;
+        NSMutableDictionary *updatedTransactions = [NSMutableDictionary dictionaryWithDictionary:transactions];
+        [updatedTransactions setObject:@(count) forKey:productIdentifier];
+        [RMStoreKeychainPersistence setTransactionsDictionary:updatedTransactions];
+        return YES;
+    } else {
+        return NO;
+    }
+}
+
+- (NSInteger)countProductOfdentifier:(NSString*)productIdentifier
+{
+    NSDictionary *transactions = [RMStoreKeychainPersistence transactionsDictionary];
+    NSInteger count = [[transactions objectForKey:productIdentifier] integerValue];
+    return count;
+}
+
+- (BOOL)isPurchasedProductOfIdentifier:(NSString*)productIdentifier
+{
+    NSDictionary *transactions = [RMStoreKeychainPersistence transactionsDictionary];
+    return [transactions objectForKey:productIdentifier] != nil;
+}
+
+- (NSSet*)purchasedProductIdentifiers
+{
+    NSDictionary *transactions = [RMStoreKeychainPersistence transactionsDictionary];
+    NSArray *productIdentifiers = [transactions allKeys];
+    return [NSSet setWithArray:productIdentifiers];
+}
+
+#pragma mark - Private
+
++ (NSDictionary*)transactionsDictionary
+{
+    NSData *data = RMKeychainGetValue(RMStoreTransactionsKeychainKey);
+    NSDictionary *transactions;
+    if (data)
+    {
+        NSError *error;
+        transactions = [NSJSONSerialization JSONObjectWithData:data options:0 error:&error];
+        NSAssert(!error, [error localizedDescription]);
+    } else {
+        transactions = [NSDictionary dictionary];
+    }
+    return transactions;
+}
+
++ (void)setTransactionsDictionary:(NSDictionary*)dictionary
+{
+    NSData *data = nil;
+    if (dictionary)
+    {
+        NSError *error;
+        data = [NSJSONSerialization dataWithJSONObject:dictionary options:0 error:&error];
+        NSAssert(!error, [error localizedDescription]);
+    }
+    RMKeychainSetValue(data, RMStoreTransactionsKeychainKey);
+}
+
+@end
