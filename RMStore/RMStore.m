12f896a4a9200657a6a616c5b7a827e53dc4e937
Alexander Edge <alex@alexedge.co.uk>
2014-10-20 13:51:23 +0100

    Add check for iOS SDk 8.0

--- RMStore/RMStore.m
+++ RMStore/RMStore.m
@@ -370,9 +370,11 @@ typedef void (^RMStoreSuccessBlock)();
             case SKPaymentTransactionStateRestored:
                 [self didRestoreTransaction:transaction queue:queue];
                 break;
+#if __IPHONE_OS_VERSION_MAX_ALLOWED >= 80000
             case SKPaymentTransactionStateDeferred:
                 [self didDeferTransaction:transaction];
                 break;
+#endif
             default:
                 break;
         }
